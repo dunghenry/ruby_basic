ac733b69c630768e76488d6ff8edb344c0773b58
dunghenry <trandungksnb6930082001@gmail.com>
2022-08-08 16:47:01 +0700

    first commit

---
+++ day01/vd01.rb
@@ -0,0 +1,38 @@
+puts("Bitwise complement")
+puts (~5) # -6
+puts(~-5) # 4
+
+puts("Boolean NOT")
+puts(!true)  #false
+puts(!false) # true
+
+puts("add operator")
+puts(10 + 20) # 30
+
+puts("subtract operator")
+puts(10 - 20) # -10
+
+puts("multiply operator")
+puts(10 * 20) # 200
+
+puts("divide operator")
+puts(10 / 20) # 0.5
+
+puts("exponential operator")
+puts(5 ** 2) # 25
+
+puts("modulo operator")
+puts(10 % 20) # 0
+
+puts("Ternary operator")
+puts(2 < 5 ? 5 : 2) # 5
+puts(5 < 2 ? 5 : 2) # 2
+
+puts("Comparison operator")   
+puts(2 == 5)   # false
+puts(2 != 5)   # true
+puts(2 > 5)    # false
+puts(2 < 5)    # true
+puts(2 >= 5)   #false
+puts(2 <= 5)   #true
+
